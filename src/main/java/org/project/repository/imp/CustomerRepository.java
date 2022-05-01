c60b13fc138927b06c573ff779ce99b481d1cd4e
arkad <mojib.khabireh@gmail.com>
2022-05-01 10:09:14 +0430

    add custom search for customer

--- src/main/java/org/project/repository/imp/CustomerRepository.java
+++ src/main/java/org/project/repository/imp/CustomerRepository.java
@@ -3,13 +3,31 @@ package org.project.repository.imp;
 import org.hibernate.SessionFactory;
 import org.project.entity.Customer;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class CustomerRepository extends GenericRepositoryImpl<Customer> implements org.project.repository.CustomerRepository {
 
 
-    private final SessionFactory sessionFactory =SessionFactorySingleton.getInstance();
-    public List<Customer> search
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
+    public List<Customer> search(Customer customer) {
+        var session = sessionFactory.getCurrentSession();
+        var criteriaBuilder = session.getCriteriaBuilder();
+        var criteriaQuery = criteriaBuilder.createQuery(Customer.class);
+        var root = criteriaQuery.from(Customer.class); // select query
+        List<Predicate> predicates = new ArrayList<>();
+        if (customer.getFirstName() != null && !customer.getEmail().isEmpty())
+            predicates.add(criteriaBuilder.equal(root.get("firstName"), customer.getFirstName()));
+
+        criteriaQuery
+                .where(predicates.toArray(new Predicate[0]));
+
+        return session.createQuery(criteriaQuery).list();
+
+    }
 
 
 }
