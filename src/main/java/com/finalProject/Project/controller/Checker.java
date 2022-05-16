b1a2c42d44c46dcf01502e2a3a19cbf80d1c63ab
arkad <mojib.khabireh@gmail.com>
2022-05-16 10:08:43 +0430

    password Checker

--- src/main/java/com/finalProject/Project/controller/Checker.java
+++ src/main/java/com/finalProject/Project/controller/Checker.java
@@ -1,5 +1,9 @@
 package com.finalProject.Project.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Pattern;
 
 public class Checker {
@@ -8,9 +12,23 @@ public class Checker {
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
                 if (Pattern.compile(regexPattern)
                         .matcher(email)
-                        .matches());
-                else
+                        .matches()) {
+                } else
                     throw new RuntimeException("invalid email");
         }
+    public void pathOfPicture(){
+     /*           System.out.println("insert path of picture: ");
+                File file = new File(path);
+                if(file.length()>299000)
+                    throw new RuntimeException("size to big");
+                InputStream avatar = new FileInputStream(file);*/
+
+        }
+        public void checkPassword(String password){
+        if (password.length()<8)
+            throw new RuntimeException("passWord invalid");
+        }
+
+
     }
 
