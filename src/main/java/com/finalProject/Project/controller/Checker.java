0d854d8d759c60146ff58bd791f36315ba2b6d36
arkad <mojib.khabireh@gmail.com>
2022-05-16 09:59:23 +0430

    add class checker for checking input add email checking

--- src/main/java/com/finalProject/Project/controller/Checker.java
+++ src/main/java/com/finalProject/Project/controller/Checker.java
@@ -1,5 +1,16 @@
 package com.finalProject.Project.controller;
 
-public class Checker {
+import java.util.regex.Pattern;
 
-}
+public class Checker {
+    public  void  email(String email){
+        String regexPattern="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+                if (Pattern.compile(regexPattern)
+                        .matcher(email)
+                        .matches());
+                else
+                    throw new RuntimeException("invalid email");
+        }
+    }
+
